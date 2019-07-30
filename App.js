6cbde94b17a0c74c0d3de278003b43e89403b91d
arifo <arifento85@gmail.com>
2019-07-31 01:30:35 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <SafeAreaView>
+        <View>
+          <Text>Todo Board</Text>
+        </View>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+export default App;
